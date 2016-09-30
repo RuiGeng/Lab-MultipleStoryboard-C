3de56735d7a030a28c281f666cf16b67738de95d
Rui Geng <ruigeng@Students-MacBook-Pro.local>
2016-09-29 21:31:54 -0400

    commit lab multiple story board

--- Lab03/ViewController.h
+++ Lab03/ViewController.h
@@ -7,9 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+NSTimer *timer;
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *labCost;
+@property double m_cost;
+@property double m_secondCost;
 
 @end
 
--- Lab03/ViewController.m
+++ Lab03/ViewController.m
@@ -14,14 +14,42 @@
 
 @implementation ViewController
 
+-(void)onTick:(NSTimer *)timer {
+    
+    _m_cost += _m_secondCost;
+    _labCost.text = [NSString stringWithFormat:@"$ %.02f",_m_cost];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _m_secondCost = 7451.98 / 15/ 10/ 60 / 60;
+    _m_cost = 0.00;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)startTimer:(id)sender {
+    
+    if(timer)
+        return;
+    
+     timer = [NSTimer scheduledTimerWithTimeInterval:1.0
+                                              target: self
+                                            selector: @selector(onTick:)
+                                            userInfo:nil
+                                            repeats:YES ];
+
+}
+
+- (IBAction)stopTimer:(id)sender {
+    if (timer){
+        [timer invalidate];
+        timer = nil;
+    }
 }
 
 @end
